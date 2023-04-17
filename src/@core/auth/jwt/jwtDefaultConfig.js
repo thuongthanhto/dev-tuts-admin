c492841c5c754d8fe5bd20b6aa698b1acd4b8aed
Thuong To <thuongthanhto@gmail.com>
2023-04-17 09:34:02 +0700

    init

---
+++ src/@core/auth/jwt/jwtDefaultConfig.js
@@ -0,0 +1,17 @@
+const apiUrl = import.meta.env.REACT_APP_API_URL;
+
+// ** Auth Endpoints
+export default {
+  loginEndpoint: '/jwt/login',
+  registerEndpoint: `${apiUrl}/auth/signup`,
+  refreshEndpoint: '/jwt/refresh-token',
+  logoutEndpoint: '/jwt/logout',
+
+  // ** This will be prefixed in authorization header with token
+  // ? e.g. Authorization: Bearer <token>
+  tokenType: 'Bearer',
+
+  // ** Value of this property will be used as key to store JWT token in storage
+  storageTokenKeyName: 'accessToken',
+  storageRefreshTokenKeyName: 'refreshToken',
+};
