d603338e13bd9da3fc1c6a948408d844d263f765
Thuong To <thuongthanhto@gmail.com>
2023-11-27 21:58:06 +0700

    Handle logout

--- src/redux/authentication/hooks/useAuthentication.js
+++ src/redux/authentication/hooks/useAuthentication.js
@@ -1,14 +1,17 @@
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import useJwt from '@src/auth/jwt/useJwt';
 import { handleLogout } from '..';
 
 export default function useAuthentication() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const logout = () => {
-    useJwt.logout();
+  const logout = async () => {
+    await useJwt.logout();
     dispatch(handleLogout());
+    navigate('/login', { replace: true });
   };
 
   return { logout };
